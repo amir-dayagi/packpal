1baeed14331c0bbe66a91b42d9b6737cafca363c
Amir Dayagi <amir.dayagi@gmail.com>
2026-02-19 13:39:10 -0800

    removed old frontend and fixed schema and dockerfile

--- frontend-old/app/hooks/useModal.ts
+++
@@ -1,6 +0,0 @@
-import { useModalContext } from "@/app_old/contexts/ModalContext"
-
-export const useModal = () => {
-    const { openModal, closeModal } = useModalContext()
-    return { openModal, closeModal }
-}
